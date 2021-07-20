a7696418e2baf43c126c2d6a22ec2e2de316364f
phantuan99 <tuanpx8@gmail.com>
2021-07-20 18:25:13 +0700

    Update food_detail

---
+++ app/src/main/java/com/example/da_mientay/ui/fooddetail/FoodDetailFragment.java
@@ -0,0 +1,38 @@
+package com.example.da_mientay.ui.fooddetail;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.da_mientay.R;
+
+public class FoodDetail extends Fragment {
+
+    private FoodDetailViewModel mViewModel;
+
+    public static FoodDetail newInstance() {
+        return new FoodDetail();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.food_detail_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(FoodDetailViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/da_mientay/ui/fooddetail/FoodDetailViewModel.java
@@ -0,0 +1,7 @@
+package com.example.da_mientay;
+
+import androidx.lifecycle.ViewModel;
+
+public class FoodDetailViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
